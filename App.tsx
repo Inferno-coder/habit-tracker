c3adc8a212262617e06fdfb43b8ee69b1d28ebb7
Pradeep <98871228+Inferno-coder@users.noreply.github.com>
2024-07-02 22:56:19 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -4,17 +4,29 @@ import About from './components/about-component/About';
 import Header from './components/header-component/Header';
 import Feature from './components/features-component/Feature';
 import AuthModal from './modalComponents/auth-modal-component/AuthModal';
-import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import DashBoard from './components/dashboard-component/DashBoard';
+
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <About/>
-      <Feature/>
       <Router>
         <Routes>
-          <Route path='/auth/signup' element={<AuthModal isOpen={true} onClose={()=>{}} type='signup' />} />
-          <Route path='/auth/login' element={<AuthModal isOpen={true} onClose={()=>{}} type='login' />} />  
+          {/* Route for '/' path */}
+          <Route path='/' element={<>
+            <Header />
+            <About />
+            <Feature />
+          </>} />
+
+          {/* Route for '/auth/signup' */}
+          <Route path='/auth/signup' element={<AuthModal isOpen={true} onClose={() => { }} type='signup' />} />
+
+          {/* Route for '/auth/login' */}
+          <Route path='/auth/login' element={<AuthModal isOpen={true} onClose={() => { }} type='login' />} />
+
+          {/* Route for '/user/:id' */}
+          <Route path='/user/:id/*' element={<DashBoard />} />
         </Routes>
       </Router>
     </div>
