5c74716dcdb67a60b26ae5924f4680a650cd0db6
Pradeep <98871228+Inferno-coder@users.noreply.github.com>
2024-07-04 22:25:00 +0530

    Create dashboardOptions.js

---
+++ dashboardOptions.js
@@ -0,0 +1,34 @@
+const dashboardOptions = [
+    {
+        title: 'Profile',
+        icon: 'fa-user' ,
+        route:'/profile'
+    },
+    {
+        title: 'Habit List',
+        icon: 'fa-list',
+        route:'/habit-list'
+    },
+    {
+        title: 'Calendar View',
+        icon: 'fa-calendar' ,
+        route:'/calendar-view'
+    },
+    {
+        title: 'Notifications and Reminders',
+        icon: 'fa-bell',
+        route:'/notifications-and-reminders'
+    },
+    {
+        title: 'Progress and Analytics',
+        icon: 'fa-chart-line' ,
+        route:'/progress-and-analytics'
+    },
+    {
+        title: 'Achievements and Rewards',
+        icon: 'fa-trophy' ,
+        route:'/achievements-and-rewards'
+    }
+];
+
+module.exports = dashboardOptions;
